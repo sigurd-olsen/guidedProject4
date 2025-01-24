ff008bc357e939f386c944aba67d7933a6db67a4
sigurd-olsen <sigurddaaolsen@gmail.com>
2025-01-24 16:43:32 +0100

    Added some psuedocode

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+// string[,] group = AssignGroup();
+
+// RandomizeAnimals();
+
+Console.WriteLine("School A");
+
+// PrintGroup(group);
