97b8b63f0d64bcb08fe48adce3be0ad861e53c7c
sigurd-olsen <sigurddaaolsen@gmail.com>
2025-01-24 17:06:29 +0100

    FInished project

--- Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d689debc8b485b7f71e775d6235937816cf391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff008bc357e939f386c944aba67d7933a6db67a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
